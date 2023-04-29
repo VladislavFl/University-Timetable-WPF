c3d7272a794ec71dd7873523b5660959ce71dbd6
VladislavFl <43980608+VladislavFl@users.noreply.github.com>
2023-04-29 12:26:59 +0300

    Project infrastructure, formation of the base class of the viewmodel

---
+++ UniversityTimetable/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace UniversityTimetable.ViewModels.Base
+{
+    // базовый класс модели представления
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string? PropertyName = null) // ссылка на поле св-ва, само значение, имя св-ва (определяется  автоматически компилятором)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
---
+++ UniversityTimetable/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniversityTimetable.ViewModels.Base;
+
+namespace UniversityTimetable.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _title = "Расписание";
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+    }
+}
